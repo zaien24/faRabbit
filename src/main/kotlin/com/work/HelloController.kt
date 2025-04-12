dfe10135d60c6fe6b797ca5243f93ecdfcc632bc
한성길 <zaien24@gmail.com>
2025-04-12 14:29:25 +0900

    [섹션 2]  6.기본 비동기 메시지 전송 part1

---
+++ src/main/kotlin/com/work/HelloController.kt
@@ -0,0 +1,13 @@
+package com.work
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello"
+    }
+}
